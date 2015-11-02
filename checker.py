cae5bf30730982d3a14df0a6adc70af4748dc7a7
Francesco d'Aloisio <francesco.daloisio@serco.com>
2015-11-02 19:30:24 +0100

    version changed

--- checker.py
+++ checker.py
@@ -3,12 +3,19 @@ import urllib
 versionFile = "https://raw.githubusercontent.com/fradaloisio/versions_test/master/VERSION"
 lastVersion = urllib.urlopen(versionFile).read()
 
-currentFile = open("VERSION", "r")
-currentVersion = currentFile.readline()
-while "VERSION" not in currentVersion:
-    currentVersion = currentFile.readline()
+lastVersion = str(lastVersion.split('\"')[1])
 
-if currentVersion is not lastVersion:
-    print("Please, upgrade")
-else:
+currentFile = open("VERSION", "r")
+#currentVersion = currentFile.readline()
+# while "VERSION" not in currentVersion:
+#     currentVersion = currentFile.readline()
+#
+# currentVersion = str(currentVersion.split('\"')[1])
+currentVersion = str(currentFile.readline())
+
+if currentVersion is lastVersion:
     print("Nice!")
+else:
+    print "Current version: " + currentVersion
+    print "Last version: " + lastVersion
+    print("Please, upgrade")
