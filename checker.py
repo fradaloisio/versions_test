6a6effc5ec273ad3e6f8f96826609bbcd225b6e0
Francesco d'Aloisio <francesco.daloisio@serco.com>
2015-11-03 17:22:12 +0100

    added version

--- checker.py
+++ checker.py
@@ -3,17 +3,18 @@ import urllib
 versionFile = "https://raw.githubusercontent.com/fradaloisio/versions_test/master/VERSION"
 lastVersion = str(urllib.urlopen(versionFile).read())
 
-#lastVersion = str(lastVersion.split('\"')[1])
+if lastVersion.__contains__("\""):
+    lastVersion = str(lastVersion.split('\"')[1])
 
 currentFile = open("VERSION", "r")
-#currentVersion = currentFile.readline()
-# while "VERSION" not in currentVersion:
-#     currentVersion = currentFile.readline()
-#
-# currentVersion = str(currentVersion.split('\"')[1])
-currentVersion = str(currentFile.readline())
+currentVersion = currentFile.readline()
+while "VERSION" not in currentVersion:
+    currentVersion = currentFile.readline()
 
-if currentVersion is lastVersion:
+if currentVersion.__contains__("\""):
+    currentVersion = str(currentVersion.split('\"')[1])
+
+if currentVersion == lastVersion:
     print("Nice!")
 else:
     print "Current version: " + currentVersion
